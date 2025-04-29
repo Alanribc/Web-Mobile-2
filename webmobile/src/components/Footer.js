0d105638ea23c44491993f95a1e402e0e5fea2e7
Alanribc <166681521+Alanribc@users.noreply.github.com>
2025-04-29 03:17:25 +0000

    fourth commit

---
+++ webmobile/src/components/Footer.js
@@ -0,0 +1,17 @@
+
+export default function Footer () {
+    return(
+        <footer>
+            <article className="artigoFooter">
+                <p>Projeto 2 de Web Mobile</p>
+                <p>Desenvolvido por:</p>
+            </article>
+            <article className="artigoFooterDois">
+                <p>André Lima 10410280</p> 
+                <p>Alan Ribeiro do Carmo 10428496</p> 
+                <p>Giovanni Suppo 10438719</p>
+                <p>Henrique Durao 10438068</p>
+            </article>
+        </footer>
+    );
+}
--- webmobile/src/components/Menu.js
+++ webmobile/src/components/Menu.js
@@ -1,13 +1,22 @@
 import Link from "next/link";
 import Tema from "./Tema";
+import Image from "next/image";
 
 export default function Menu() {
     return (
         <nav>
-            <ul>
-                <li><Link href = "/">Home</Link></li>
-                <li><Link href = "/">Sobre</Link></li>
-            </ul>
+            <section className = "logomenu">
+                <Image 
+                src = "/images/play.png"
+                width = {80}
+                height = {80}
+                alt = "Logo"
+                />
+                <ul>
+                    <li><Link href = "/">Home</Link></li>
+                    <li><Link href = "/">Sobre</Link></li>
+                </ul>
+            </section>
             <Tema />
         </nav>
     );
