fac9a67ff1ec1cfc2b659d54b5ca94b7117ec378
Alanribc <166681521+Alanribc@users.noreply.github.com>
2025-05-03 17:30:11 +0000

    nono commit

--- webmobile/src/app/page.js
+++ webmobile/src/app/page.js
@@ -7,6 +7,19 @@ export default function Home() {
         <p>Aqui, você poderá encontrar os filmes que você mais gosta e fazer uma lista com eles.</p>
         <p>Através da API, você poderá consultar uma grande diversidade de filmes</p>
       </article>
+      <article className = "article2">
+        <section>
+          <input
+          type = "text"
+          placeholder="Digite o nome do filme:"
+          //value:
+          //onChange= 
+          >
+          </input>
+        </section>
+        <p>Nome:{}</p>
+        
+      </article>
     </main>
     
   );
