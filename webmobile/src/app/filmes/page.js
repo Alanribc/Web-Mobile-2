9a04d12f42e5004bb9c23faa62a836b67a59c401
Alanribc <166681521+Alanribc@users.noreply.github.com>
2025-05-15 14:52:54 +0000

    aba filmes

--- webmobile/src/app/filmes/page.js
+++ webmobile/src/app/filmes/page.js
@@ -3,9 +3,9 @@ import Image from "next/image";
 export default function Filme() {
 
     const filmes = [
-        {codigo: 1, nome: "Shrek 1", ano: 2001, descricao: "Shrek e Burro", capa: "/images/velozes.jpg"},
+        {codigo: 1, nome: "Shrek 1", ano: 2001, descricao: "Shrek e Burro", capa: "/images/shrek.jpg"},
         {codigo: 2, nome: "Velozes e Furiosos 1", ano: 2001, descricao: "carro", capa: "/images/velozes.jpg"},
-        {codigo: 3, nome: "EuroTrip", ano: 2004, descricao: "besteirol", capa: "/images/velozes.jpg"}
+        {codigo: 3, nome: "EuroTrip", ano: 2004, descricao: "besteirol", capa: "/images/eurotrip.jpg"}
     ]
 
     const qtdFilmes = filmes.reduce((acumulador) => {
@@ -16,17 +16,17 @@ export default function Filme() {
         <main className="mainPrincipal">
             <article className="article1">
                 <h1>Filmes: {qtdFilmes}</h1>
-                <section>
+                <section className="capaFilmes">
                         {
                             filmes.map((f) => {
                                 return(
                                     <div key = {f.codigo} className="divFilmes">
-                                        <h2 key={f.codigo}>{f.codigo} - {f.nome}</h2>
                                         <Image
                                         width= {200}
-                                        height= {200}
+                                        height= {300}
                                         src = {f.capa}
                                         alt = {f.nome}
+                                        className="capa"
                                         />
                                     </div>
                                     
